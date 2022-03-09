a7f1c656c3ddc0bcc84795c9fa9923ac13ec6cc7
jaeesu <wogmlsu@gmail.com>
2022-03-10 01:21:31 +0900

    feat: 토큰 재발급 api 추가 에러 핸들링을 위한 전역 예외 처리 함수 추가

--- src/main/java/com/example/amattang/controller/UserController.java
+++ src/main/java/com/example/amattang/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.amattang.controller;
 
 import com.example.amattang.payload.reponse.LoginResponseDto;
+import com.example.amattang.payload.reponse.ReIssueTokenResponseDto;
 import com.example.amattang.payload.request.LoginRequestDto;
+import com.example.amattang.payload.request.ReIssueTokenRequestDto;
 import com.example.amattang.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -25,6 +27,13 @@ public class UserController {
     public ResponseEntity<?> loginByAccessToken(@RequestBody @Valid LoginRequestDto dto) {
         LoginResponseDto loginResponseDto = userService.loadUser(dto);
         return succes(loginResponseDto, "로그인 성공");
+    }
+
+    @ApiOperation(value = "토큰 재발급", response = ReIssueTokenResponseDto.class)
+    @PostMapping("/issue/re")
+    public ResponseEntity<?> reIssueToken(@RequestBody @Valid ReIssueTokenRequestDto dto) {
+        ReIssueTokenResponseDto response = userService.reIssueToken(dto);
+        return succes(response, dto.getRequest() + " 토큰 재발급 성공");
     }
 
 
---
+++ src/main/java/com/example/amattang/payload/request/ReIssueTokenRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.amattang.payload.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class ReIssueTokenRequestDto {
+
+    @ApiModelProperty(value = "요청하는 토큰", example = "access / refresh")
+    @NotBlank(message = "요청하는 토큰 형식이 누락되었습니다.")
+    private String request;
+
+    @NotBlank(message = "토큰 값이 누락되었습니다.")
+    @ApiModelProperty(value = "토큰 값", example = "Bearer + 토큰 값 (사이 공백)")
+    private String token;
+}
