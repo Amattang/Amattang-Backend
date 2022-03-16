e865bf73c7d9a34bda679de3ce34a548b189d69b
jaeesu <wogmlsu@gmail.com>
2022-03-16 12:24:52 +0900

    docs: image controller 문서 수정

--- src/main/java/com/example/amattang/controller/ImageController.java
+++ src/main/java/com/example/amattang/controller/ImageController.java
@@ -32,7 +32,7 @@ public class ImageController {
     @ApiOperation(value = "4-1. 체크리스트에 이미지 저장")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "checkListId", value = "이미지를 저장할 체크리스트 아이디", paramType = "path", dataType = "int"),
-            @ApiImplicitParam(name = "image", value = "저장할 이미지 리스트(form-data) / 저장할 이미지 s3 url 리스트(json)", dataType = "MultiPartFile")
+            @ApiImplicitParam(name = "image", value = "저장할 이미지 리스트", dataType = "form-data")
     })
     @PostMapping
     public ResponseEntity<?> saveImage(@CurrentUser UserPrincipal userPrincipal, @PathVariable("checkListId") Long checkListId, @ModelAttribute List<MultipartFile> image) {
@@ -56,7 +56,7 @@ public class ImageController {
     @ApiOperation(value = "4-3. 체크리스트의 이미지 삭제")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "checkListId", value = "이미지를 저장할 체크리스트 아이디", paramType = "path"),
-            @ApiImplicitParam(name = "imageIds", value = "삭제할 이미지 아이디 리스트", paramType = "param")
+            @ApiImplicitParam(name = "imageId", value = "삭제할 이미지 아이디 리스트", paramType = "param")
     })
     @DeleteMapping
     public ResponseEntity<?> deleteImage(@CurrentUser UserPrincipal userPrincipal, @PathVariable("checkListId") Long checkListId, @RequestParam("imageId") List<Long> imageIds) {
