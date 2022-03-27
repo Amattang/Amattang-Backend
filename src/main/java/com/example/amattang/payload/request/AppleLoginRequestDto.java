39ab13b6d5f109dbbabed0c0226c76e5a966c5bb
jaeesu <wogmlsu@gmail.com>
2022-03-27 19:01:50 +0900

    feat: 로깅 수정

--- src/main/java/com/example/amattang/payload/request/AppleLoginRequestDto.java
+++ src/main/java/com/example/amattang/payload/request/AppleLoginRequestDto.java
@@ -13,6 +13,7 @@ public class AppleLoginRequestDto {
     @ApiModelProperty(name = "애플에서 제공하는 사용자 아이디", example = "1234.sijdc9i03e.1234")
     private String user;
 
+    @NotBlank(message = "email은 빈 값이 허용되지 않습니다.")
     @Email(message = "이메일 형식이 잘못되었습니다.")
     @ApiModelProperty(name = "사용자 이메일", example = "jaery@gmail.com")
     private String email;
