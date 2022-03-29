5a7eba0c0f0d171c630c3167ccb0623e6b845f4d
서재희 <50989437+jaeesu@users.noreply.github.com>
2022-03-30 01:21:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,7 @@ BUILD_JAR=$(ls /home/ubuntu/deploy/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build : $JAR_NAME" >> /home/ubuntu/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/
-cp $BUILD_JAR $DEPLOY_PATH
+DEPLOY_PATH=/home/ubuntu/deploy/
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
