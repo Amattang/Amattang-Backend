ab105feda15098e63ffb88ca5da95e9f5bff43dc
서재희 <50989437+jaeesu@users.noreply.github.com>
2022-03-30 10:45:53 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,4 +21,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-sudo java -DSpring.profiles.active=prod -Duser.timezone=Asia/Seoul -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
+nohup java -DSpring.profiles.active=prod -Duser.timezone=Asia/Seoul -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
